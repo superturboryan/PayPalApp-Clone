32f5ed30b0d9e1aa0f64476a1e99a098458ea827
Ryan David Forsyth <ryan@ryans-macbook-air.local>
2019-02-25 11:05:06 -0500

    Create UI main screen using ScrollView, embedded UIViews, StackViews, set auto-layout constraints, set corner-radius and shadows programmatically, set UIImages and views.

--- PayPalApp/ViewController.swift
+++ PayPalApp/ViewController.swift
@@ -15,10 +15,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var balanceView: UIView!
+    
+    @IBOutlet weak var getMoreView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        getMoreView.layer.cornerRadius = CGFloat(5.0)
+        balanceView.layer.cornerRadius = CGFloat(5.0)
+        
+        balanceView.layer.shadowOpacity = Float(0.2)
+        balanceView.layer.shadowOffset = CGSize(width: 2, height: 1)
+        balanceView.layer.shadowRadius = CGFloat(2)
+        
+        var rightBarItemImage = UIImage(named: "settings_icon")
+        
+        rightBarItemImage = rightBarItemImage?.withRenderingMode(.alwaysOriginal)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: rightBarItemImage, style: .plain, target: nil, action: nil)
+        
+        let profileImmageView = UIImageView(image: UIImage(named: "profile_image"))
+        
+        self.navigationItem.titleView = profileImmageView
+        
+        var leftBarItemImage = UIImage(named: "notifications_icon")
+        
+        leftBarItemImage = leftBarItemImage?.withRenderingMode(.alwaysOriginal)
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: leftBarItemImage, style: .plain, target: nil, action: nil)
+        
         
         
     }
